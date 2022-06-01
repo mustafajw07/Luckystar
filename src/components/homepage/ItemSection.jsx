d92ea660dc2982f58faed7ca2fd5592e920ff11d
mustafa jawadwala <mustafajw07@gmail.com>
2022-06-01 23:07:30 +0530

    done

--- src/components/homepage/ItemSection.jsx
+++ src/components/homepage/ItemSection.jsx
@@ -15,11 +15,11 @@ return (
                     img="https://m.media-amazon.com/images/I/51eo0ObG8aL.jpg" />
                 <Item title="DORSET" description="Digital door lock" link="dorset"
                     img="https://www.dorsetindia.com/Uploads/ProductsImage/801.jpg" />
-                <Item title="GODREJ" description="Navtal 7 levers 4 Keys Padlock" link="godrej"
+                <Item title="GODREJ" description="Navtal 7 levers Padlock" link="godrej"
                     img="https://m.media-amazon.com/images/I/5144inlQMFL._SX522_.jpg" />
                 <Item title="HAFELE" description="Tandem Box" link="hafele"
                     img="https://5.imimg.com/data5/BR/NP/MY-36244974/hafele-draw-hyderabad-500x500.jpg" />
-                <Item title="EBCO" description="Furniture Fittings And Accessories" link="ebco"
+                <Item title="EBCO" description="Furniture Fittings" link="ebco"
                     img="https://ebco.in/image/cache/catalog/Table%20Bracket/table-bracket-550x550.jpg" />
                 <Item title="PALLADIUM" description="Hinges" link="palladium"
                     img="https://5.imimg.com/data5/SELLER/Default/2020/11/YL/VM/TL/13312678/palladium-hinges-towerbolts-500x500.png" />
